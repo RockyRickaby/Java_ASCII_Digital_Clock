26e7a5f9806f323a3d376b70160755cae9bf7643
Maurício Alcântara da Luz <iamthefreeman@outlook.com.br>
2024-01-25 11:16:56 -0300

    now it's possible to set up the clock within the program without having to use command line arguments

--- src/App.java
+++ src/App.java
@@ -6,31 +6,80 @@ import clock.Clock;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean[] arguments = new boolean[3];
+        Clock cloc = null;
         if (args.length > 0) {
+            boolean[] arguments = new boolean[3];
             for (int i = 0; i < args.length && i < arguments.length; i++) {
                 arguments[i] = args[i].equals("1");
             }
+            cloc = new Clock(arguments[0]).start(arguments[1], arguments[2]);
         } else {
+            boolean currTime, _24h, showSecs;
+
             Scanner in = new Scanner(System.in);
-            System.out.println("Use current system time? (y/n)");
+            System.out.println("Use 24 hour clock? (y/n)");
             String input = in.next().toLowerCase();
             in.nextLine();
-            arguments[0] = input.matches("^y.*");
-
-            System.out.println("Use 24 hour clock? (y/n)");
-            input = in.next().toLowerCase();
-            in.nextLine();
-            arguments[1] = input.matches("^y.*");
+            _24h = input.matches("^y.*");
 
             System.out.println("Show seconds? (y/n)");
             input = in.next().toLowerCase();
             in.nextLine();
-            arguments[2] = input.matches("^y.*");
-            in.close();
+            showSecs = input.matches("^y.*");
+
+            System.out.println("Use current system time? (y/n)");
+            input = in.next().toLowerCase();
+            in.nextLine();
+            currTime = input.matches("^y.*");
+
+            if (currTime) {
+                cloc = new Clock(currTime).start(_24h, showSecs);
+            } else {
+                int hour = -1,
+                minute = -1,
+                second = -1;
+                while(hour < 0) {
+                    System.out.print("Set clock's hour to: ");
+                    if (!in.hasNextInt()) {
+                        in.nextLine();
+                        continue;
+                    }
+                    hour = in.nextInt() % 24;
+                }
+                in.nextLine();
+                if (!_24h) {
+                    String res = "";
+                    while (!res.equals("am") && !res.equals("pm")) {
+                        System.out.print("AM or PM? ");
+                        res = in.nextLine().toLowerCase();
+                    }
+                    hour = hour % 12;
+                    if (res.equals("pm")) {
+                        hour += 12;  
+                    }
+                }
+                while (minute < 0) {
+                    System.out.print("Set clock's minutes to: ");
+                    if (!in.hasNextInt()) {
+                        in.nextLine();
+                        continue;
+                    }
+                    minute = in.nextInt() % 60;
+                }
+                while (second < 0) {
+                    System.out.print("Set clock's seconds to: ");
+                    if (!in.hasNextInt()) {
+                        in.nextLine();
+                        continue;
+                    }
+                    second = in.nextInt() % 60;
+                }
+                cloc = new Clock((hour * 3600) + (minute * 60) + second).start(_24h, showSecs);
+            }
+            in.close();    
         }
-        Clock cloc = new Clock(arguments[0]).start(arguments[1], arguments[2]);
         while (cloc.isRunning()) {
+            
         }
     }
 }
